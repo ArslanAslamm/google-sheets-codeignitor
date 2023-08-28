7c8399bfb54e557dc24272c0c117f3177924855c
jiyasom <36759455+jiyasom@users.noreply.github.com>
2023-08-28 17:05:09 +0400

    first commit

---
+++ cloud_whatsap.js
@@ -0,0 +1,7 @@
+api_key = AIzaSyClB_9o50rXgdHPeELmqybh_GPrWZ4OWHc;
+client_id =
+  "558077595426-5u4jq8hs79q93if77fkv7ech4sq0jqo2.apps.googleusercontent.com";
+
+client_secret = "GOCSPX - PxC7iEFnxMV0F8U3GE7Yfm50gHt2;";
+
+email_address = "cloud-whatsapp-service@cloud-whatsapp.iam.gserviceaccount.com";
